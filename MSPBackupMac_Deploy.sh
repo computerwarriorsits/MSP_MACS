40ccb1564f206c56d3062ca8e88a85938d7df56b
Khristos <khristos666@gmail.com>
2020-03-20 15:46:04 -0400

    Update MSPBackupMac_Deploy.sh

--- MSPBackupMac_Deploy.sh
+++ MSPBackupMac_Deploy.sh
@@ -2,4 +2,4 @@
 FILE1=$1
 curl -O https://raw.githubusercontent.com/CW-Khristos/MSP_MACS/master/mxb-macosx-x86_64.pkg
 mv mxb-macosx-x86_64.pkg $FILE1
-installer -dumplog -pkg $FILE1 -target /
+echo '1234' | sudo installer -dumplog -pkg $FILE1 -target /
